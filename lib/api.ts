0d3bfe4b2c80f6ca1e3959d67e3d096e1b182105
Khizer-shah88 <khizershah493@gmail.com>
2026-04-25 22:12:28 +0500

    domain

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API_BASE_URL =
-	process.env.NEXT_PUBLIC_API_URL || 'https://developershub-backend.onrender.com';
+	process.env.NEXT_PUBLIC_API_URL || 'https://developershubbackend-production.up.railway.app';
 
 const api = axios.create({
 	baseURL: API_BASE_URL,
